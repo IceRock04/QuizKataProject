8749d44fe324972d1bbd0e682744fa2c61285d61
Jacob Minikel <jaminikel@gmail.com>
2024-02-28 18:40:41 -0600

    In Progress - Basics of GUI Application are complete

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,57 @@
 package src;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import src.quiz.Quiz;
+
 /**
  *
  */
 public class Controller {
 
+    @FXML
+    private Button answer1;
+    @FXML
+    private Button answer2;
+    @FXML
+    private Button answer3;
+    @FXML
+    private Button answer4;
+    @FXML
+    private Label timerText;
+    @FXML
+    private Label scoreText;
+    @FXML
+    private Label questionText;
+
+
+    public Controller(){
+
+    }
+
+    /**
+     * When an answer button is clicked on, the handleCheckAnswer() method will grab the answer text provided.
+     * The grabbed text will be sent to the Quiz class to check if it is the correct answer.
+     * @param actionEvent
+     */
+    public void handleCheckAnswer(ActionEvent actionEvent) {
+        //Grabs the reference of the button that was just clicked
+        Button button = (Button) actionEvent.getTarget();
+        //Grabs the text of the button, which will be passed through the Quiz class to check if it is the right answer
+        //System.out.println(button.getText());
+        //This next line will return a String. The response will either say that the user got the correct answer, or it will say that the answer is wrong and show the true answer.
+        //quiz.checkAnswer(button.getText());
+
+    }
+
+    private void changeQuestion() {
+        //Code here to change the text on all the buttons/labels that relate to a question (Label to show the question, and the text on the buttons to show answers)
+    }
+    //Put some stuff here. Maybe load the Quiz object here and work from there?
+    public void initialize() {
+        //this.quiz = new Quiz();
+        questionText.textProperty().set("Hello");
+    }
 }
--- src/QuizApplication.java
+++ src/QuizApplication.java
@@ -7,7 +7,12 @@
 package src;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 /**
  * This is the driver class for the program.
@@ -28,6 +33,9 @@ public class QuizApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("QuizQuestion-Layout.fxml")));
+        stage.setTitle("Quiz Application");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
